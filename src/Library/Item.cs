bc838ddcfc0394769cb51bb9794a3a79e8904c81
giovannamandirola <69222964+giovannamandirola@users.noreply.github.com>
2020-09-18 13:44:59 -0300

    Arreglos

--- src/Library/Item.cs
+++ src/Library/Item.cs
@@ -5,9 +5,36 @@ namespace Library
     public class Item
     {
         public string Name { get; set; }
-        public int Damage { get; set; }
-        public int Defense { get; set; }
-
+        private int _damage;
+        public int Damage 
+        { 
+            get
+            {
+                return this._damage;
+            }
+            set
+            {
+                if(value>=0)
+                {
+                    this._damage = value;
+                }
+            } 
+        }
+        private int _defense;
+        public int Defense
+        { 
+            get
+            {
+                return this._defense;
+            }
+            set
+            {
+                if(value>=0)
+                {
+                    this._defense = value;
+                }
+            } 
+        }
         public Item (string name, int damage, int defense)
         {
             this.Name = name;
